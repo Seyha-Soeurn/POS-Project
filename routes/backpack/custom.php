0f4e5ebbf5283f858e18e6cc6a63c2fbe59210fe
Seyha-Soeurn <seyha.soeurn@student.passerellesnumeriques.org>
2022-11-16 13:36:57 +0700

    #3:Crud Purchase

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -21,4 +21,5 @@ Route::group([
     Route::crud('user', 'UserCrudController');
     Route::crud('supplier', 'SupplierCrudController');
     Route::crud('product', 'ProductCrudController');
+    Route::crud('purchase', 'PurchaseCrudController');
 }); // this should be the absolute last line of this file
