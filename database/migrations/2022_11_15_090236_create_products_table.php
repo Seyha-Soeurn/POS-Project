c202761446560ae77ab045d68f3268afe1e0983e
sok github <sokgithub@gmail.com>
2022-11-16 11:14:51 +0700

    #2 table update table product to make easier in crud proccess

--- database/migrations/2022_11_15_090236_create_products_table.php
+++ database/migrations/2022_11_15_090236_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string("price")->nullable();
-            $table->boolean('stock')->nullable();
+            $table->string('stock')->nullable();
             $table->timestamps();
         });
     }
